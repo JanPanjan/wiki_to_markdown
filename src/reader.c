a39925f253120939c885cea52da52e8a386dbd5d
JanPanjan <89231282@student.upr.si>
2025-05-09 12:54:56 +0200

    popravu indentation

--- src/reader.c
+++ src/reader.c
@@ -8,33 +8,35 @@ const unsigned int MAX_BUFFER_SIZE = 2000;
 Buffer *get_filenames(const char *dir) {}
 
 Buffer *read_file_contents(const char *fname) {
-	FILE *file = fopen(fname, "r");
-	if (file == NULL) {
-		handle_result((Result){.file = __FILE_NAME__,
-			.linenr = __LINE__,
-			.error_code = READER_FAIL,
-			.context = "error opening file"}
-		);
-	}
+    FILE *file = fopen(fname, "r");
+    if (file == NULL) {
+        handle_result((Result){
+            .file = __FILE_NAME__,
+            .linenr = __LINE__,
+            .error_code = READER_FAIL,
+            .context = "error opening file"
+        });
+    }
 
-	Buffer *content = malloc(sizeof(Buffer));
-	content->raw = malloc(MAX_BUFFER_SIZE);
+    Buffer *content = malloc(sizeof(Buffer));
+    content->raw = malloc(MAX_BUFFER_SIZE);
 
-	int i = 0;
-	char c;
+    int i = 0;
+    char c;
 
-	while ((c = fgetc(file)) != EOF) {
-		content->raw[i] = c;
-		i++;
-	}
+    while ((c = fgetc(file)) != EOF) {
+        content->raw[i] = c;
+        i++;
+    }
 
-	fclose(file); // ker smo pridni
+    fclose(file); // ker smo pridni
 
-	handle_result((Result){.file = __FILE_NAME__,
-		.linenr = __LINE__,
-		.error_code = SUCCESS,
-		.context = "successfully read file"}
-	);
+    handle_result((Result){
+        .file = __FILE_NAME__,
+        .linenr = __LINE__,
+        .error_code = SUCCESS,
+        .context = "successfully read file"
+    });
 
-	return content;
+    return content;
 }
